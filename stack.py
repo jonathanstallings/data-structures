226342b27f24ef0ba4e9c065323b5e002a033889
jay-tyler <jmtyler@gmail.com>
2015-06-25 15:35:39 -0700

    start to Stack class

---
+++ stack.py
@@ -0,0 +1,23 @@
+from __future__ import unicode_literals
+from linked_list import LinkedList, Node
+
+class Stack():
+
+    def __init__(self, iterable=()):
+        self.other = LinkedList()
+        self.other.__init__(iterable)
+
+    def __repr__(self):
+        return self.other.__repr__()
+
+    def push(self, value):
+        """Will add a value to the stack"""
+        self.other.insert(value)
+
+    def pop(self):
+        """Will remove val from stack and return"""
+        try:
+            val = self.other.pop()
+
+        return val
+
