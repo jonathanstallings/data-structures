760fa6ac8a9d2a371d1ac3dadd60e789028eb483
Jonathan Stallings <jonathanstallings@outlook.com>
2015-06-28 18:56:56 -0700

    Sketch docstring for parenthetics functions

---
+++ parenthetics.py
@@ -0,0 +1,15 @@
+from __future__ import unicode_literals
+
+from stack import Stack
+
+
+def parenthetics(string):
+    """Examine string for valid use of paretheses.
+    args:
+        string: the unicode string to Examine
+    returns:
+        1 if the string is "open"
+        0 if the string is "balanced"
+       -1 if the string is "broken"
+    """
+    pass
