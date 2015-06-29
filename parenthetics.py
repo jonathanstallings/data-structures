ce2d5ea4e72f7488f9d51657e20f2c29a2afa611
Jonathan Stallings <jonathanstallings@outlook.com>
2015-06-28 19:42:53 -0700

    Complete first pass of parenthical function

--- parenthetics.py
+++ parenthetics.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 from stack import Stack
 
 
-def parenthetics(string):
+def parenthetical(string):
     """Examine string for valid use of paretheses.
     args:
         string: the unicode string to Examine
@@ -12,4 +12,16 @@ def parenthetics(string):
         0 if the string is "balanced"
        -1 if the string is "broken"
     """
-    pass
+    open_parens = Stack()
+    for char in unicode(string):
+        if char == '(':
+            open_parens.push(char)
+        elif char == ')':
+            try:
+                open_parens.pop()
+            except IndexError:
+                return -1
+    if len(open_parens) > 1:
+        return 1
+    else:
+        return len(open_parens)
