e5c8ddbfb4015e7675b7e87c68444e39261de089
Jonathan Stallings <jonathanstallings@outlook.com>
2015-07-04 14:12:41 -0700

    Add test for maxheap sort

--- test_binary_heap.py
+++ test_binary_heap.py
@@ -9,6 +9,12 @@ valid_constructor_args = [
     [[1243.12235, 13262.3, 26523.15, 98653.234], 1243.12235]
 ]
 
+valid_constructor_args_max = [
+    ["qzdfwqefnsadnfoiaweod", "z"],
+    [[6, 7, 9, 4, 2, 1, 56, 8, 0, 43523], 43523],
+    [[1243.12235, 13262.3, 26523.15, 98653.234], 98653.234]
+]
+
 pop_constructors = [
     [5, 8, 98, 43, 21, 1, 3, 7, 0, 3, 4, 7, 2345, 4, 64],
     [33, 5314, 124, 243, 234, 1324, 2, 342, 1243, 134],
@@ -21,10 +27,41 @@ invalid_constructors = [
     ]
 
 
+def minheap_sorted(heap):
+    for i in range(len(heap)):
+        try:
+            if heap[i] > heap[(2*i + 1)]:
+                return False
+            if heap[i] > heap[(2*i) + 2]:
+                return False
+        except IndexError:
+            return True
+
+
+def maxheap_sorted(heap):
+    for i in range(len(heap)):
+        try:
+            if heap[i] < heap[(2*i + 1)]:
+                return False
+            if heap[i] < heap[(2*i) + 2]:
+                return False
+        except IndexError:
+            return True
+
+
 @pytest.mark.parametrize("input, output", valid_constructor_args)
-def test_valid_instantiation(input, output):
+def test_valid_instantiation_min(input, output):
     """Test instantiation by creating and doing one pop"""
     heap_under_test = BinaryHeap(input)
+    assert minheap_sorted(heap_under_test)
+    assert heap_under_test.pop() == output
+
+
+@pytest.mark.parametrize("input, output", valid_constructor_args_max)
+def test_valid_instantiation_max(input, output):
+    """Test instantiation by creating and doing one pop"""
+    heap_under_test = BinaryHeap(input, minheap=False)
+    assert maxheap_sorted(heap_under_test)
     assert heap_under_test.pop() == output
 
 
@@ -36,7 +73,7 @@ def test_invalid_instantiation(bad_input):
 
 
 def test_push1():
-    """  First push single list of [9, 5, 2, 1, 0, 7] """
+    """  First push single item from list of [9, 5, 2, 1, 0, 7] """
     heap_under_test = BinaryHeap()
     heap_under_test.push(9)
     assert heap_under_test.pop() == 9
