c2a2da2c021d597979b448fa1544a52ea48d8b05
Jonathan Stallings <jonathanstallings@outlook.com>
2015-07-04 19:58:21 -0700

    First pass at script

--- priorityq.py
+++ priorityq.py
@@ -19,20 +19,32 @@ class QNode(object):
         """Implement this and following method with logic to compare
         priority and value appropiately.
         """
-        pass
+        if self.priority is None and other.priority is None:
+            self.value == other.value
+        elif self.priority is None or other.priority is None:
+            return False
+        else:
+            self.priority == other.priority
 
     def __lt__(self, other):
         """Implement in tandem with __eq__."""
-        pass
+        if self.priority is None and other.priority is None:
+            self.value < other.value
+        elif self.priority is None or other.priority is None:
+            self.priority > other.priority  # Since None is less than anything
+        else:
+            self.priority < other.priority
 
 
 class PriorityQ(object):
     """A class for a priority queue. Compose this from BinaryHeap."""
     def __init__(self, iterable=()):
         """We can iteratively use insert here."""
-        pass
+        self.heap = BinaryHeap(iterable=())
+        for item in iterable:
+            self.insert(item)
 
-    def insert(item):  # Want to extend spec to add priority as 2nd optional arg
+    def insert(self, item, priority=None):  # Want to extend spec to add priority as 2nd optional arg
         """Insert an item into the queue. Would be nice to examine item as follows:
         If item is node:
             add to PriorityQ
@@ -40,7 +52,10 @@ class PriorityQ(object):
             init QNode with item as val and priority as None
             add to PriorityQ
         """
-        pass
+        if isinstance(item, QNode):
+            self.heap.push(item)
+        else:
+            self.heap.push(QNode(item, priority))
 
     def pop():
         """Remove the most important item from the queue."""
