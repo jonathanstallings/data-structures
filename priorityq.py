784ea9b4fdfa2587e20fb12f4b80a9758f0a3e4d
Jonathan Stallings <jonathanstallings@outlook.com>
2015-07-04 21:22:34 -0700

    Improve docstrings

--- priorityq.py
+++ priorityq.py
@@ -8,6 +8,10 @@ from binary_heap import BinaryHeap
 class QNode(object):
     """A class for a queue node."""
     def __init__(self, val, priority=None):
+        """Initialize a QNode with a value and an optional priority.
+
+        Priority must be an integer, highest being 0.
+        """
         self.val = val
         self.priority = priority
 
@@ -20,9 +24,7 @@ class QNode(object):
         return "{val}, Priority:{p}".format(val=self.val, p=self.priority)
 
     def __eq__(self, other):
-        """Implement this and following method with logic to compare
-        priority and val appropiately.
-        """
+        """Overloads equality comparison to check priority, then value."""
         if self.priority == other.priority:
             return self.val == other.val
         elif self.priority is None or other.priority is None:
@@ -31,7 +33,7 @@ class QNode(object):
             return self.priority == other.priority
 
     def __lt__(self, other):
-        """Implement in tandem with __eq__."""
+        """Overloads lesser than comparison to check priority, then value."""
         if self.priority == other.priority:
             return self.val < other.val
         elif self.priority is None:
@@ -43,9 +45,17 @@ class QNode(object):
 
 
 class PriorityQ(object):
-    """A class for a priority queue. Compose this from BinaryHeap."""
+    """A class for a priority queue."""
     def __init__(self, iterable=()):
-        """We can iteratively use insert here."""
+        """Initialize a priority queue, optionally with items from iterable.
+
+        The items in the priority queue are stored in a binary minheap. Items
+        first sorted by priority, then value. Priority is expressed as an
+        integer with 0 being the most important.
+
+        args:
+            iterable: an optional iterable to add to the priority queue.
+        """
         self.heap = BinaryHeap(iterable=())
         for item in iterable:
             self.insert(item)
@@ -66,12 +76,15 @@ class PriorityQ(object):
         self.heap[index] = value
 
     def insert(self, item, priority=None):
-        """Insert an item into the queue. Would be nice to examine item as follows:
-        If item is node:
-            add to PriorityQ
-        else:
-            init QNode with item as val and priority as None
-            add to PriorityQ
+        """Insert an item into the priority queue.
+
+        If the item is a QNode object, it will be added as is.
+        If not, a new QNode object is created to hold the item with optional
+        priority assigned.
+
+        args:
+            item: the item to add (QNode or other value)
+            priority: the optional integer priority (0 is most important)
         """
         if isinstance(item, QNode):
             self.heap.push(item)
@@ -79,9 +92,9 @@ class PriorityQ(object):
             self.heap.push(QNode(item, priority))
 
     def pop(self):
-        """Remove the most important item from the queue."""
+        """Remove and return the most important item from the queue."""
         return self.heap.pop()
 
     def peek(self):
-        """Returns the most important item from queue without removal."""
+        """Return the most important item from queue without removal."""
         return self.heap[0]
