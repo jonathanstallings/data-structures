d6950bda561f0524d1f0c8c5955281995cba6daa
Jonathan Stallings <jonathanstallings@outlook.com>
2015-07-06 22:23:11 -0700

    Refactor a bit

--- graph.py
+++ graph.py
@@ -26,7 +26,7 @@ class Graph(object):
 
     def nodes(self):
         """Return a list of all nodes in the graph."""
-        return [node for node in self.graph]
+        return [node for node in self]
 
     def edges(self):
         """Return a list of all edges in the graph."""
@@ -34,30 +34,31 @@ class Graph(object):
 
     def add_node(self, n):
         """Add a new node to the graph."""
-        self.graph[n] = set()
+        self[n] = set()
 
     def add_edge(self, n1, n2):
         """Add a new edge connecting n1 to n2."""
-        self.graph[n1].add(n2)
+        self[n1].add(n2)
 
     def del_node(self, n):
         """Delete a node from the graph."""
-        del self.graph[n]
+        del self[n]
         for edgeset in self.graph.values():
             edgeset.discard(n)
 
     def del_edge(self, n1, n2):
         """Delete the edge connecting two nodes from graph."""
-        self.graph[n1].remove(n2)
+        self[n1].remove(n2)
 
     def has_node(self, n):
         """Check if a given node is in the graph."""
-        return n in self.graph
+        return n in self
 
     def neighbors(self, n):
         """Return a list of all nodes connected to 'n' by edges."""
         neighbors = []
-        for node in self.graph:
-            if n in self.node:
+        for node in self:
+            if n in self[node]:
                 neighbors.append(node)
         return neighbors
+
