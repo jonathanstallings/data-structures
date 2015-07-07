34a04d7df8753928045e76c82935bd53636064c9
Jonathan Stallings <jonathanstallings@outlook.com>
2015-07-06 23:50:55 -0700

    Add nodes if not exist with add_edge method

--- graph.py
+++ graph.py
@@ -30,6 +30,10 @@ class Graph(object):
 
     def add_edge(self, n1, n2):
         """Add a new edge connecting n1 to n2."""
+        if not self.has_node(n1):
+            self.add_node(n1)
+        if not self.has_node(n2):
+            self.add_node(n2)
         self[n1].add(n2)
 
     def del_node(self, n):
