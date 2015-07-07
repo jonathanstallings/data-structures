2b9603fcf03f92febb11a04421e8921ed28a1978
Jonathan Stallings <jonathanstallings@outlook.com>
2015-07-07 13:58:09 -0700

    Mark areas for refactor

--- graph.py
+++ graph.py
@@ -6,7 +6,7 @@ class Graph(object):
     def __init__(self):
         self.graph = {}
 
-    def __repr__(self):
+    def __repr__(self):  # Consider how we want to repr this.
         return repr(self.graph)
 
     def __len__(self):
@@ -21,12 +21,14 @@ class Graph(object):
     def __setitem__(self, index, value):
         self.graph[index] = value
 
-    def __delitem__(self, index):
+    def __delitem__(self, index):  # Add cleanup
         del self.graph[index]
 
     def add_node(self, n):
         """Add a new node to the graph."""
-        self.graph.setdefault(n, set())  # Good! But should warn on 2nd add?
+        if not self.has_node(n):
+            raise KeyError('Node already in graph.')
+        self[n] = set()
 
     def add_edge(self, n1, n2):
         """Add a new edge connecting n1 to n2."""
@@ -41,7 +43,7 @@ class Graph(object):
     def del_node(self, n):
         """Delete a node from the graph."""
         del self[n]
-        for edgeset in self.graph.values():
+        for edgeset in self.graph.values():  # Move cleanup to __delitem__
             edgeset.discard(n)
 
     def del_edge(self, n1, n2):
@@ -59,7 +61,7 @@ class Graph(object):
     def iter_edges(self):
         for node in self:
             for edge in self[node]:
-                yield [node, edge]
+                yield (node, edge)
 
     def edges(self):
         return list(self.iter_edges())
@@ -70,9 +72,7 @@ class Graph(object):
                 yield node
 
     def neighbors(self, n):
-        if not self.has_node(n):
-            raise KeyError('Node is not in the graph')
-        return list(self.iter_neighbors(n))
+        return self[n]
 
     def adjacent(self, n1, n2):
         """Check if there is an edge connecting 'n1' and 'n2'."""
