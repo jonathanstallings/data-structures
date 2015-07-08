d3983108a4dd2545f13ccaef4d56c5b411ed2b6d
Jonathan Stallings <jonathanstallings@outlook.com>
2015-07-07 22:35:29 -0700

    Complete outlined tests

--- test_graph.py
+++ test_graph.py
@@ -106,3 +106,29 @@ def test_add_edge_n1_n2_exist_without_edges(graph_filled):
     assert n1 in g and n2 in g
     assert n2 in g[n1]
     assert len(g[n1]) == 1 and len(g[n2]) == 0
+
+
+def test_del_node_exists(graph_filled):
+    g = graph_filled
+    g.del_node(5)
+    assert 5 not in g
+    assert 5 not in g.graph.values()
+
+
+def test_del_node_empty_error(graph_empty):
+    with pytest.raises(KeyError):
+        graph_empty.del_node(10)
+
+
+def test_del_edge_exists(graph_filled):
+    g = graph_filled
+    n1, n2 = 10, 5
+    g.del_edge(n1, n2)
+    assert n1 in g and n2 in g
+    assert n2 not in g[n1]
+
+
+def test_del_edge_not_exist(graph_filled):
+    with pytest.raises(KeyError):
+        graph_filled.del_edge(100, 200)
+
