686f0245f333c59c0b4260e0aa701452071d3d3a
Jonathan Stallings <jonathanstallings@outlook.com>
2015-07-20 13:58:59 -0700

    Update test fixtures with weighted dictionary structure

--- test_graph.py
+++ test_graph.py
@@ -14,12 +14,12 @@ def graph_empty():
 def graph_filled():
     g = Graph()
     g.graph = {
-        5: set([10]),
-        10: set([5, 20, 15]),
-        15: set(),
-        20: set([5]),
-        25: set(),
-        30: set()
+        'A': {'B': 10},
+        'B': {'A': 5, 'D': 15, 'C': 20},
+        'C': {},
+        'D': {'A': 5},
+        'E': {},
+        'F': {}
     }
     return g
 
@@ -28,13 +28,13 @@ def graph_filled():
 def graph_filled_for_traversal():
     g = Graph()
     g.graph = {
-        1: set([2, 3]),
-        2: set([4, 5]),
-        3: set([6, 7]),
-        4: set([]),
-        5: set([3]),
-        6: set([5]),
-        7: set([6])
+        'A': {'B': 10, 'C': 15},
+        'B': {'D': 15, 'E': 5},
+        'C': {'F': 50, 'G': 25},
+        'D': {},
+        'E': {'C': 5},
+        'F': {'E': 10},
+        'G': {'F': 20}
     }
     return g
 
