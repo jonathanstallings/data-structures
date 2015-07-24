53354200eb8f758550bef33af3e5f3a44953110b
Jonathan Stallings <jonathanstallings@outlook.com>
2015-07-24 00:46:44 -0700

    Add further insert tests

--- test_bst.py
+++ test_bst.py
@@ -33,15 +33,73 @@ def fixed_setup():
 
 
 def test_init_empty():
-    new = Node()
-    assert new.val is None
-    assert new.left is None and new.right is None
+    root = Node()
+    assert root.val is None
+    assert root.left is None and root.right is None
 
 
 def test_init_with_val():
-    new = Node(10)
-    assert new.val == 10
-    assert new.left is None and new.right is None
+    root = Node(10)
+    assert root.val == 10
+    assert root.left is None and root.right is None
+
+
+def test_insert_in_empty_root():
+    root = Node()
+    expected = 10
+    root.insert(expected)
+    actual = root.val
+    assert expected == actual
+
+
+def test_insert_lesser_in_filled_root():
+    root = Node(10)
+    expected = 5
+    root.insert(expected)
+    actual = root.left.val
+    assert expected == actual
+    assert root.right is None
+
+
+def test_insert_greater_in_filled_root():
+    root = Node(10)
+    expected = 15
+    root.insert(expected)
+    actual = root.right.val
+    assert expected == actual
+    assert root.left is None
+
+
+def test_insert_lesser_in_filled_tree1():
+    root = Node(10)
+    root.left, root.right = Node(5), Node(15)
+    root.insert(1)
+    assert root.left.left.val == 1
+    assert root.left.right is None
+
+
+def test_insert_lesser_in_filled_tree2():
+    root = Node(10)
+    root.left, root.right = Node(5), Node(15)
+    root.insert(7)
+    assert root.left.right.val == 7
+    assert root.left.left is None
+
+
+def test_insert_greater_in_filled_tree1():
+    root = Node(10)
+    root.left, root.right = Node(5), Node(15)
+    root.insert(17)
+    assert root.right.right.val == 17
+    assert root.right.left is None
+
+
+def test_insert_greater_in_filled_tree2():
+    root = Node(10)
+    root.left, root.right = Node(5), Node(15)
+    root.insert(12)
+    assert root.right.left.val == 12
+    assert root.right.right is None
 
 
 def test_insert(rand_setup):
