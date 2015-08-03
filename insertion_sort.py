f3cb538c2b908a41ddf8c562401103f6f3496f83
Jonathan Stallings <jonathanstallings@outlook.com>
2015-08-03 14:12:06 -0700

    Improve docstrings

--- insertion_sort.py
+++ insertion_sort.py
@@ -1,4 +1,20 @@
+"""This module contains the insertion sort method, which performs an
+in-place sort on a passed in list. Insertion sort has a best case timeit
+complexity of O(n) when list is nearly sorted, and a worst case of O(n2)
+when the incoming list is already reverse sorted. While this not always
+the most efficient algorithm, it is still popular when the data is nearly
+sorted (because it is adaptive) or when the problem size is small.
+See the excellent 'sortingalgorithms.com' for more information.
+
+"""
+
+
 def insertion_sort(un_list):
+    """Sort a list in place using insertion sort.
+
+    args:
+        un_list: the list to sort
+    """
     if type(un_list) is not list:
         return "You must pass a valid list as argument. Do it."
 
