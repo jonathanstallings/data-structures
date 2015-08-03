57a02c9e3bb0ed82ee84a08dbadba0dac4e7f2f4
Jonathan Stallings <jonathanstallings@outlook.com>
2015-08-03 15:06:51 -0700

    Add insertion sort with duplicate values test

--- test_insertion_sort.py
+++ test_insertion_sort.py
@@ -13,6 +13,15 @@ def test_insertion_sort():
     assert expected == actual
 
 
+def test_insertion_sort_with_duplicates():
+    expected = [1, 3, 3, 6, 7, 8, 8, 8]
+    unsorted = expected[:]
+    shuffle(unsorted)
+    in_sort(unsorted)
+    actual = unsorted
+    assert expected == actual
+
+
 def test_insertion_sort_wrong_type():
     with pytest.raises(TypeError):
         in_sort('some string')
