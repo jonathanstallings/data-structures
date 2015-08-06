37e09a2c8d6f49d4b164a87e2ff6a6a5afe20c52
Jonathan Stallings <jonathanstallings@outlook.com>
2015-08-06 14:46:50 -0700

    Add performance demo

--- radix_sort.py
+++ radix_sort.py
@@ -1,7 +1,22 @@
-"""Jonny Five's Docstrings are alive!"""
+"""This module contains the radix sort method (radix_srt), which performs an
+in-place sort on a passed in list. Radix sort has a best case time
+complexity of O(n log n. This implementation of radix sort is stable, but not
+adaptive.
+
+References:
+
+Radix Sort in Python
+http://www.geekviewpoint.com/python/sorting/radixsort
+by Isai Damier
+"""
 
 
 def radix_srt(un_list):
+    """Sort a list in place with radix sort.
+
+    args:
+        un_list: the list to be sorted.
+    """
     radix = 10
     maxLen = False
     tmp, placement = -1, 1
@@ -27,4 +42,25 @@ def radix_srt(un_list):
 
 
 if __name__ == '__main__':
-    pass
+    from random import randint
+    bigints = [randint(9000, 10000) for x in range(1, 10001)]
+    smallints = [randint(0, 9) for x in range(1, 10001)]
+    nums = range(0, 10001)
+    BEST_CASE = smallints
+    WORST_CASE = bigints
+
+    from timeit import Timer
+
+    SETUP = """from __main__ import BEST_CASE, WORST_CASE, radix_srt"""
+
+    best = Timer('radix_srt({})'.format(BEST_CASE), SETUP).timeit(100)
+
+    worst = Timer('radix_srt({})'.format(WORST_CASE), SETUP).timeit(100)
+
+    print("""
+        Best case represented as a list with integers between 0 and 9.
+        Worst case represented as a list with large integers (here, between
+        9000 and 9999).
+        """)
+    print('Best Case: {}'.format(best))
+    print('Worst Case: {}'.format(worst))
